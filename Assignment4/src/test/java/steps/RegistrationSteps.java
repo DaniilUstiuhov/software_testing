6c512e3e701e6a7ca867fcace77412e2fe1e643c
DaniilUstiuhov <molot2004sf@gmail.com>
2026-06-07 22:03:42 +0300

    Add Assignment4: Cucumber + Selenium student registration form automation

---
+++ Assignment4/src/test/java/steps/RegistrationSteps.java
@@ -0,0 +1,80 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class RegistrationSteps {
+
+    private WebDriver driver;
+
+    @Given("Users launch Chrome browser")
+    public void launchBrowser() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @When("Users open the registration form webpage")
+    public void openWebpage() {
+        driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+    }
+
+    @Then("Users enter name {string}")
+    public void enterName(String name) {
+        driver.findElement(By.cssSelector("input[placeholder='First Name']")).sendKeys(name);
+    }
+
+    @And("Users enter email {string}")
+    public void enterEmail(String email) {
+        driver.findElement(By.cssSelector("input[placeholder='name@example.com']")).sendKeys(email);
+    }
+
+    @And("Users select gender {string}")
+    public void selectGender(String gender) {
+        driver.findElement(By.xpath("//label[normalize-space()='" + gender + "']/preceding-sibling::input[@type='radio']")).click();
+    }
+
+    @And("Users enter mobile number {string}")
+    public void enterMobile(String mobile) {
+        driver.findElement(By.cssSelector("input[placeholder='Enter Mobile Number']")).sendKeys(mobile);
+    }
+
+    @And("Users enter date of birth {string}")
+    public void enterDob(String dob) {
+        // using JS to set date value reliably (format: yyyy-MM-dd)
+        WebElement dateInput = driver.findElement(By.cssSelector("input[type='date']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].value='" + dob + "'", dateInput);
+    }
+
+    @And("Users enter subject {string}")
+    public void enterSubject(String subject) {
+        driver.findElement(By.cssSelector("input[placeholder='Enter Subject']")).sendKeys(subject);
+    }
+
+    @And("Users select hobby {string}")
+    public void selectHobby(String hobby) {
+        driver.findElement(By.xpath("//label[normalize-space()='" + hobby + "']/preceding-sibling::input[@type='checkbox']")).click();
+    }
+
+    @And("Users enter current address {string}")
+    public void enterAddress(String address) {
+        driver.findElement(By.cssSelector("textarea")).sendKeys(address);
+    }
+
+    @And("Users click Submit button")
+    public void clickSubmit() {
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+    }
+
+    @After
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ Assignment4/src/test/resources/features/registration.feature
@@ -0,0 +1,14 @@
+Feature: Student Registration Form Automation
+
+  Scenario: Fill and submit the student registration form
+    Given Users launch Chrome browser
+    When Users open the registration form webpage
+    Then Users enter name "Daniil"
+    And Users enter email "daniil@example.com"
+    And Users select gender "Male"
+    And Users enter mobile number "1234567890"
+    And Users enter date of birth "2000-01-15"
+    And Users enter subject "Computer Science"
+    And Users select hobby "Sports"
+    And Users enter current address "123 Main Street, City"
+    And Users click Submit button
